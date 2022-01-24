70bd715bb185b7a8801ef4ef46313061ea0c8bb7
axel <axel@pop-os.localdomain>
2022-01-24 13:52:36 +0100

    add options to cors middleware

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const port = process.env.PORT
 connectDB()
 
 // Middlewares
-app.use(cors())
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cookieParser())
