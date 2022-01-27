18876bce45c4c482869ccb7fbbd977a68b59c074
axel <axel@pop-os.localdomain>
2022-01-27 19:02:37 +0100

    connect users to socket server

--- socket/index.js
+++ socket/index.js
@@ -1,10 +1,23 @@
-const io = require ('socket.io')(9000, {
+const io = require ('socket.io')(8000, {
     cors: {
         origin: "http://localhost:3000"
     }
 })
 
+let users = []
+
+// Avoid adding a user each time there is a connection to the application, it should only be done once
+const addUser = (token, socketId) => {
+    !users.some((user) => user.token === token) &&
+    users.push({token, socketId})
+}
+
 io.on("connection", (socket) => {
     console.log("a user connected")
-    io.emit("weclcome", "hello, this is socket server")
+    // Take token and socketId from user
+    socket.on("addUser", token => {
+        addUser(token, socket.id)
+        // Send users to client
+        io.emit("getUsers", users)
+    })
 })
