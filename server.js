0ff66715187c23e0027e6f2acbcde707fe2a05a1
Dyl-Richardson <dylanrichardson1606@gmail.com>
2022-01-28 23:31:10 +0100

    trying to fix fetch

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ connectDB()
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, "/client/build")));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, "client/build", "index.html"))
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, "client/build/index.html"))
     })
 }
 else {
