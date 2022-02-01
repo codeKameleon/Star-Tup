9b01faa83a0ae13265df1e2b4b3587147d626c44
Dyl-Richardson <dylanrichardson1606@gmail.com>
2022-02-01 15:37:00 +0100

    add update message

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -34,12 +34,12 @@ export default function Login() {
             <button className='px-5 py-4 text-white'><i className="fas fa-chevron-left"></i></button>
           </Link>
           <img src="logo.png" alt="logo" className='w-3/4 m-auto mb-10' />
-          {error.length > 0 ? <p className='text-[#b33232] text-sm m-auto text-center'>{error}</p> : null}
         </header>
 
 
         <form onSubmit={(e) => handleSubmit(e)} className='mt-0 space-y-4 flex flex-col'>
           <h2 className='mt-6 text-center text-2xl font-extrabold text-white'>Login to your account</h2>
+        <p className='text-[#fa5f5f] text-xl m-auto text-center'>{error}</p>
           <label htmlFor="email" className='sr-only'>Email</label>
           <input type="email" autoComplete='on' name='email' onChange={e => setData({ ...data, email: e.target.value })} required className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm' placeholder="Email address" />
 
@@ -50,6 +50,7 @@ export default function Login() {
           <button type='submit' className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-[#7aa5d2] hover:bg-[#6798cc] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>Login</button>
           <p className='text-white text-center'>Don't have an account ? <Link to="/register" className='text-[#7aa5d2] hover:text-[#6798cc]'>Register</Link></p>
         </form>
+        
       </main>
     </>
   );
