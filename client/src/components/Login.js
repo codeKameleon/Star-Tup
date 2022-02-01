1c6c601ed9c0bf9ae44d473ddf6b8d15cf1b3209
Dyl-Richardson <dylanrichardson1606@gmail.com>
2022-02-01 16:51:20 +0100

    add root redirect

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -18,10 +18,10 @@ export default function Login() {
   const handleSubmit = e => {
     e.preventDefault()
 
-    axios.post("http://localhost:9000/api/account/login", data, { credentials: 'same-origin' })
+    axios.post("/api/account/login", data, { credentials: 'same-origin' })
       .then(res => {
         setCookie('userId', res.data.user_id)
-        navigate("/app/chat", { replace: true })
+        navigate("/chat", { replace: true })
       })
       .catch(err => setError(err.response.data.message))
   }
