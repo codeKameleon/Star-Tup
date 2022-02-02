a546d33c3d09af7be17e10b8391b6f9ead065676
Dyl-Richardson <dylanrichardson1606@gmail.com>
2022-02-02 10:08:12 +0100

    update route

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -21,7 +21,7 @@ export default function Login() {
     axios.post("/api/account/login", data, { credentials: 'same-origin' })
       .then(res => {
         setCookie('userId', res.data.user_id)
-        navigate("app/chat", { replace: true })
+        navigate("/app/chat", { replace: true })
       })
       .catch(err => setError(err.response.data.message))
   }
@@ -35,10 +35,11 @@ export default function Login() {
           </Link>
           <img src="../logo.png" alt="logo" className='w-3/4 m-auto mb-10' />
         </header>
-{/* 
-        <div>
-          <p className='text-[#fa5f5f] text-xl m-auto text-center'>Error wrong password</p>
-        </div> */}
+        {error.length > 0 ?
+          <div className='absolute w-3/4 text-center mt-72'>
+            <p className='text-[#fa5f5f] text-xl'>{error}</p>
+          </div>
+          : null}
 
         <form onSubmit={(e) => handleSubmit(e)} className='mt-0 space-y-4 flex flex-col'>
           <h2 className='mt-6 text-center text-2xl font-extrabold text-white'>Login to your account</h2>
@@ -52,7 +53,7 @@ export default function Login() {
           <button type='submit' className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-[#7aa5d2] hover:bg-[#6798cc] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>Login</button>
           <p className='text-white text-center'>Don't have an account ? <Link to="/app/register" className='text-[#7aa5d2] hover:text-[#6798cc]'>Register</Link></p>
         </form>
-        
+
       </main>
     </>
   );
