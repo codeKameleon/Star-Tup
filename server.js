481a84f98ce99582789aeea737e4a1efe8f199e2
axel <axel@pop-os.localdomain>
2022-02-03 10:50:48 +0100

    fix redirects (in progress)

--- server.js
+++ server.js
@@ -132,11 +132,11 @@ app.get('/api', (req, res) => res.send({
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, "/client/build")));
 
-    app.get("/", (req, res) => {
+    app.get("*", (req, res) => {
         res.redirect("/app/")
     })
 
-    app.get('/app/*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname, "client/build/index.html"))
     })
 }
